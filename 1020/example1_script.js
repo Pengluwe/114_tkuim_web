2a34c3bd3c3297effc084484b2b0baccdd53d305
unknown <ericpeng0928@gmail.com>
2025-10-24 22:43:43 +0800

    1020

---
+++ 1020/example1_script.js
@@ -0,0 +1,15 @@
+// example1_script.js
+// 傳統語法：僅使用 var、function、字串串接
+
+// 顯示提示窗
+alert('歡迎來到 JavaScript！');
+
+// 在 Console 顯示訊息
+console.log('Hello JavaScript from console');
+
+// 在頁面指定區域輸出文字
+var el = document.getElementById('result');
+el.textContent = '這行文字是由外部 JS 檔案寫入的。';
+var el2 = document.getElementById('result');
+el2.textContent = '412630062 彭律瑋';
+
